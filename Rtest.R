10b245ef7bca1b1e41683699cd404777c8eccadc
Sam Trejo <samtrejo@alumni.stanford.edu>
2020-06-30 12:44:11 -0700

    Add R file

---
+++ Rtest.R
@@ -0,0 +1,7 @@
+install.packages("installr")
+library(installr)
+updateR()
+
+install.packages("usethis")
+library(usethis)
+use_git_config(user.name = "Sam Trejo", user.email = "samtrejo@alumni.stanford.edu")
